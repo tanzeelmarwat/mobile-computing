be5d4694e9323165270f1b27663018d5f9d80d04
Tanzeel Ur Rehman <tanzeelmarwat@gmail.com>
2018-09-16 22:32:32 +0500

    Communication between Activities

--- app/src/main/java/com/tanzeelmarwat/mobilecomputing/HomeActivity.java
+++ app/src/main/java/com/tanzeelmarwat/mobilecomputing/HomeActivity.java
@@ -1,13 +1,31 @@
 package com.tanzeelmarwat.mobilecomputing;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView tvHelloWorld;
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mContext = this;
+        tvHelloWorld = findViewById(R.id.tv_hello_world);
+        tvHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Text View Hello World Click Listener
+                Intent intent = new Intent(mContext, UniversityActivity.class);
+                intent.putExtra("message", "I am coming from home");
+                startActivity(intent);
+            }
+        });
     }
 }
